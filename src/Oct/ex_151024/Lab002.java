0a038757130e499538122a422b5e3428bfe953df
ankit.singh <singh.ankit6690@gmail.com>
2024-10-15 06:40:05 +0530

    Ternary_Operator

---
+++ src/Oct/ex_151024/Lab002.java
@@ -0,0 +1,14 @@
+package Oct.ex_151024;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        //if score is greater than 90 then grade A
+        //if score is greater than 75 then grade B
+        //if score is greater than 50 then grade C
+        //if score is greater than 35 then grade D
+        //if score is less than 35 then Fail
+        int Score = 45;
+        String grade = Score>90 ? "A":(Score>75 ? "B":(Score>50 ? "C":(Score>35 ?"D":"FAIL")));
+        System.out.println(grade);
+    }
+}
